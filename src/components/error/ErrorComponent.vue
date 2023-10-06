a2c3732a1e1c2ecc4a9fb78f12cc40a8418f6e96
KarpAAA <ivankarpinskij.ua@gmail.com>
2023-10-06 11:01:13 +0300

    pizza time_creation now rounded 403 error_page to forgive not working cook have access

---
+++ src/components/error/ErrorComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="background-color: #131e3a; height: 100vh;width: 100vw">
+    <div class="error_page">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ErrorComponent"
+}
+</script>
+
+<style scoped>
+.error_page{
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
